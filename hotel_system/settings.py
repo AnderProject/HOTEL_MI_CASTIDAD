5701a7b66d337e107b1bb7628e38a10b89d77576
Batman <merchanbalcazaranderson@gmail.com>
2025-04-06 22:21:11 -0500

    new

--- hotel_system/settings.py
+++ hotel_system/settings.py
@@ -36,7 +36,6 @@ MESSAGE_TAGS = {
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.renserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gestion',
+    
 ]
 
 
